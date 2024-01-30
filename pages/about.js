eb20cd0c38c0e77aefbe2b91992128f91af663cf
Ibuki Uehara <n23003@std.it-college.ac.jp>
2024-01-30 07:09:11 +0000

    5.5
    アバウトページに画像を表示
    about.jsにImageとeyecatchコンポーネントをインポートし、ヒーローの中でイメージの設定
    画像を優先的に読み込むpriorityとプレースホルダとしてブラー画像を表示する

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,30 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
+import Contact from 'components/contact'
 import {
   TwoColumn,
   TwoColumnMain,
   TwoColumnSidebar
 } from 'components/two-column'
-import Contact from 'components/contact'
+import Image from 'next/legacy/image'
+import eyecatch from 'images/about.jpg'
 
 const About = () => {
   return (
     <Container>
       <Hero title='About' subtitle='About development activities' />
+
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=''
+          layout='responsive'
+          sizes='(min-width: 1152px) 1152px, 100vw'
+          priority
+          placeholder='blur'
+        />
+      </figure>
 
       <TwoColumn>
         <TwoColumnMain>
